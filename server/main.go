30b33ae156d239ef8c6bbecbaa6bab1b720236ae
Rui Wang <4a9ebb44@gmail.com>
2024-09-02 19:04:15 +0200

    feat(main.go): add init of `audiencemanager`

--- server/main.go
+++ server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"moqlivestream/component/audiencemanager"
 	"moqlivestream/component/channelmanager"
 	"moqlivestream/server/webtransportserver"
 )
 
 func main() {
 	channelmanager.InitChannelManager()
+	audiencemanager.InitAudienceManager()
 
 	go func() { webtransportserver.StartServer() }()
 	select {}

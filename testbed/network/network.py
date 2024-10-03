421755e38acf55d5e58c5a43b741174ff9670495
Rui Wang <4a9ebb44@gmail.com>
2024-10-03 22:35:40 +0200

    feat(testbed): revamp network setup

--- testbed/network/network.py
+++ testbed/network/network.py
@@ -6,206 +6,79 @@ from pyroute2.netlink.exceptions import NetlinkError
 NAMESPACES = [
     {
         "name": "ns1",
-        "routes": [
-            {
-                "dst": "10.0.2.0/24",
-                "gateway": "10.0.1.2",
-                "if": "v1p2",
-            },
-        ],
     },
     {
         "name": "ns2",
-        "routes": [
-            {
-                "dst": "10.0.1.0/24",
-                "gateway": "10.0.2.1",
-                "if": "v2p1",
-            },
-            {
-                "dst": "10.0.3.0/24",
-                "gateway": "10.0.2.2",
-                "if": "v2p2",
-            },
-            {
-                "dst": "10.0.4.0/24",
-                "gateway": "10.0.2.2",
-                "if": "v2p2",
-            },
-        ],
     },
     {
         "name": "ns3",
-        "routes": [
-            {
-                "dst": "10.0.2.0/24",
-                "gateway": "10.0.3.2",
-                "if": "v3p2",
-            },
-            {
-                "dst": "10.0.4.0/24",
-                "gateway": "10.0.3.2",
-                "if": "v3p2",
-            },
-        ],
-    },
-    {
-        "name": "ns4",
-        "routes": [
-            {
-                "dst": "10.0.2.0/24",
-                "gateway": "10.0.4.1",
-                "if": "v4p1",
-            },
-            {
-                "dst": "10.0.3.0/24",
-                "gateway": "10.0.4.1",
-                "if": "v4p1",
-            },
-            {
-                "dst": "10.0.5.0/24",
-                "gateway": "10.0.4.2",
-                "if": "v4p2",
-            },
-        ],
-    },
-    {
-        "name": "ns5",
-        "routes": [
-            {
-                "dst": "10.0.4.0/24",
-                "gateway": "10.0.5.1",
-                "if": "v5p1",
-            },
-        ],
     },
 ]
 
 BRIDGES = [
     {
         "name": "br1",
-        "address": "10.0.5.253",
-        "mask": 24,
+    },
+    {
+        "name": "br2",
     },
 ]
 
 DEVICES = [
     {
-        "name": "v1p1",  # streamer
-        # "peer": "",
+        "name": "v1p1",  # streamer iface
+        "peer": "v1p2",
         "ns": "ns1",
         "ip": "10.0.1.1",
         "mask": 24,
         "broadcast": "10.0.1.255",
-        # "bridge": "",
+        "bridge": "br1",
     },
     {
-        "name": "v1p2",
-        "peer": "v2p1",
-        "ns": "ns1",
-        "ip": "10.0.1.2",
-        "mask": 24,
-        "broadcast": "10.0.1.255",
-        # "bridge": "",
-    },
-    {
-        "name": "v2p1",
-        "peer": "v1p2",
+        "name": "v2p1",  # server iface
+        "peer": "v2p2",
         "ns": "ns2",
         "ip": "10.0.2.1",
         "mask": 24,
         "broadcast": "10.0.2.255",
-        # "bridge": "",
+        "bridge": "br1",
     },
     {
-        "name": "v2p2",
+        "name": "v3p1",  # br connection iface on br1
         "peer": "v3p2",
-        "ns": "ns2",
-        "ip": "10.0.2.2",
-        "mask": 24,
-        "broadcast": "10.0.2.255",
-        # "bridge": "",
+        "bridge": "br2",
     },
     {
-        "name": "v2p2",
-        "peer": "v4p1",
-        "ns": "ns2",
-        "ip": "10.0.2.2",
-        "mask": 24,
-        "broadcast": "10.0.2.255",
-        # "bridge": "",
+        "name": "v3p2",  # br connection iface on br2
+        "peer": "v3p1",
+        "bridge": "br1",
     },
     {
-        "name": "v3p1",  # server
-        # "peer": "",
+        "name": "v4p1",  # audience 1 iface
+        "peer": "v4p2",
         "ns": "ns3",
         "ip": "10.0.3.1",
         "mask": 24,
         "broadcast": "10.0.3.255",
-        # "bridge": "",
+        "bridge": "br2",
     },
     {
-        "name": "v3p2",
-        "peer": "v2p2",
+        "name": "v5p1",  # audience 2 iface
+        "peer": "v5p2",
         "ns": "ns3",
         "ip": "10.0.3.2",
         "mask": 24,
         "broadcast": "10.0.3.255",
-        # "bridge": "",
+        "bridge": "br2",
     },
     {
-        "name": "v3p2",
-        "peer": "v4p1",
+        "name": "v6p1",  # audience 3 iface
+        "peer": "v6p2",
         "ns": "ns3",
-        "ip": "10.0.3.2",
+        "ip": "10.0.3.3",
         "mask": 24,
         "broadcast": "10.0.3.255",
-        # "bridge": "",
-    },
-    {
-        "name": "v4p1",
-        "peer": "v2p2",
-        "ns": "ns4",
-        "ip": "10.0.4.1",
-        "mask": 24,
-        "broadcast": "10.0.4.255",
-        # "bridge": "",
-    },
-    {
-        "name": "v4p1",
-        "peer": "v3p2",
-        "ns": "ns4",
-        "ip": "10.0.4.1",
-        "mask": 24,
-        "broadcast": "10.0.4.255",
-        # "bridge": "",
-    },
-    {
-        "name": "v4p2",
-        "peer": "v5p1",
-        "ns": "ns4",
-        "ip": "10.0.4.2",
-        "mask": 24,
-        "broadcast": "10.0.4.255",
-        # "bridge": "",
-    },
-    {
-        "name": "v5p1",
-        "peer": "v4p2",
-        "ns": "ns5",
-        "ip": "10.0.5.1",
-        "mask": 24,
-        "broadcast": "10.0.5.255",
-        "bridge": "br1",
-    },
-    {
-        "name": "br1",  # bridge
-        # "peer": "",
-        "ns": "ns5",
-        "ip": "10.0.5.254",
-        "mask": 24,
-        "broadcast": "10.0.5.255",
-        # "bridge": "",
+        "bridge": "br2",
     },
 ]
 
@@ -254,7 +127,6 @@ def create_bridge():
                 ipr.link("add", ifname=br["name"], kind="bridge")
                 dev = ipr.link_lookup(ifname=br["name"])[0]
                 ipr.link("set", index=dev, state="up")
-                ipr.addr("add", index=dev, address=br["address"], mask=br["mask"])
             except Exception as e:
                 print(f"error: {e}, while creating bridge: {br}")
 
@@ -274,37 +146,34 @@ def create_iface():
     with IPRoute() as ipr:
         for config in DEVICES:
             try:
-                # Create the veth pair only if a peer is defined
-                if config.get("peer"):
-                    ipr.link(
-                        "add", ifname=config["name"], kind="veth", peer=config["peer"]
-                    )
-                    peer = ipr.link_lookup(ifname=config["peer"])[0]
+                # Create veth pair
+                ipr.link("add", ifname=config["name"], kind="veth", peer=config["peer"])
+                peer = ipr.link_lookup(ifname=config["peer"])[0]
 
-                    # Set the bridge master only if a bridge is defined
-                    if config.get("bridge"):
-                        br = ipr.link_lookup(ifname=config["bridge"])[0]
-                        ipr.link("set", index=peer, master=br)
+                # Set bridge master
+                br = ipr.link_lookup(ifname=config["bridge"])[0]
+                ipr.link("set", index=peer, master=br)
 
                 # Lookup the device
                 dev = ipr.link_lookup(ifname=config["name"])[0]
-                ipr.link("set", index=dev, net_ns_fd=config["ns"])
 
-                # Configure namespace
-                ns = NetNS(config["ns"])
-                ns.addr(
-                    "add",
-                    index=dev,
-                    address=config["ip"],
-                    mask=config["mask"],
-                    broadcast=config["broadcast"],
-                )
-                ns.link("set", index=dev, state="up")
-                lo = ns.link_lookup(ifname="lo")[0]
-                ns.link("set", index=lo, state="up")
-                ns.close()
+                # config device in namespace
+                if config.get("ns"):
+                    ipr.link("set", index=dev, net_ns_fd=config["ns"])
+                    ns = NetNS(config["ns"])
+                    ns.addr(
+                        "add",
+                        index=dev,
+                        address=config["ip"],
+                        mask=config["mask"],
+                        broadcast=config["broadcast"],
+                    )
+                    ns.link("set", index=dev, state="up")
+                    lo = ns.link_lookup(ifname="lo")[0]
+                    ns.link("set", index=lo, state="up")
+                    ns.close()
 
-                # Bring up the peer if it exists
+                # bring up the bridge iface
                 if config.get("peer"):
                     ipr.link("set", index=peer, state="up")
 
@@ -326,36 +195,22 @@ def remove_iface():
                 print(f"{e}: ", type(e).__name__)
 
 
-def create_routes():
-    for namespace in NAMESPACES:
-        for route in namespace["routes"]:
-            try:
-                ns = NetNS(namespace["name"])
-                # iface = ns.link_lookup(ifname=route['if'])[0]
-                ns.route("add", dst=route["dst"], gateway=route["gateway"])
-                ns.close()
-            except Exception as e:
-                print(e, namespace, route)
+def add_delay(if_name, delay_us):
+    with IPRoute() as ipr:
+        dev = ipr.link_lookup(ifname=if_name)[0]
+        ipr.tc("add", "netem", index=dev, handle="1:", delay=delay_us)
 
 
-def add_delay(ns_name, if_name, delay_us):
-    with NetNS(ns_name) as ns:
-        dev = ns.link_lookup(ifname=if_name)[0]
-        ns.tc("add", "netem", index=dev, handle="1:", delay=delay_us)
+def remove_delay(if_name):
+    with IPRoute() as ipr:
+        dev = ipr.link_lookup(ifname=if_name)[0]
+        ipr.tc("del", index=dev, handle="1:")
 
 
-def remove_delay(ns_name, if_name):
-    if len(netns.listnetns()) == 0:
-        return
-    with NetNS(ns_name) as ns:
-        dev = ns.link_lookup(ifname=if_name)[0]
-        ns.tc("del", index=dev, handle="1:")
-
-
-def add_bandwidth_limit(ns_name, if_name, rate, latency, burst):
-    with NetNS(ns_name) as ns:
-        dev = ns.link_lookup(ifname=if_name)[0]
-        ns.tc(
+def add_bandwidth_limit(if_name, rate, latency, burst):
+    with IPRoute() as ipr:
+        dev = ipr.link_lookup(ifname=if_name)[0]
+        ipr.tc(
             "add",
             "tbf",
             index=dev,
@@ -367,24 +222,49 @@ def add_bandwidth_limit(ns_name, if_name, rate, latency, burst):
         )
 
 
-def remove_bandwidth_limit(ns_name, if_name):
-    with NetNS(ns_name) as ns:
-        dev = ns.link_lookup(ifname=if_name)[0]
-        ns.tc("del", index=dev, handle="0:", parent="1:")
+def remove_bandwidth_limit(if_name):
+    with IPRoute() as ipr:
+        dev = ipr.link_lookup(ifname=if_name)[0]
+        ipr.tc("del", index=dev, handle="0:", parent="1:")
 
 
 def setup_tc():
-    add_delay()
-    add_bandwidth_limit()
+    # streamer to server
+    add_delay("v1p2", 10000)
+    add_bandwidth_limit("v1p2", "10mbit", "50ms", "10000")
+
+    # server to audience bridge (br2)
+    add_delay("v3p2", 10000)
+    add_bandwidth_limit("v3p2", "10mbit", "50ms", "10000")
+
+    # individual audience iface on br2
+    add_delay("v4p2", 5000)
+    add_bandwidth_limit("v4p2", "5mbit", "10ms", "10000")
+
+    add_delay("v5p2", 10000)
+    add_bandwidth_limit("v5p2", "10mbit", "30ms", "10000")
+
+    add_delay("v6p2", 10000)
+    add_bandwidth_limit("v6p2", "20mbit", "50ms", "10000")
 
 
 def clear_tc():
     try:
-        remove_bandwidth_limit()
+        remove_bandwidth_limit("v1p2")
+        remove_bandwidth_limit("v3p2")
+
+        remove_bandwidth_limit("v4p2")
+        remove_bandwidth_limit("v5p2")
+        remove_bandwidth_limit("v6p2")
     except Exception as e:
         print(e)
     try:
-        remove_delay()
+        remove_delay("v1p2")
+        remove_delay("v3p2")
+
+        remove_delay("v4p2")
+        remove_delay("v5p2")
+        remove_delay("v6p2")
     except Exception as e:
         print(e)
 
@@ -393,7 +273,6 @@ def setup():
     create_ns()
     create_bridge()
     create_iface()
-    create_routes()
 
 
 def clean():
